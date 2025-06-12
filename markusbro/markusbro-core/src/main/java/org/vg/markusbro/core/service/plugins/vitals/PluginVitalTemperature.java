2cc548f1fee398722e60b5346f2199b97d4df18d
Vira Gaidar <Vira.Gaidar@gmail.com>
2025-06-12 23:29:15 +0200

    Temperature reporting implemented

--- markusbro/markusbro-core/src/main/java/org/vg/markusbro/core/service/plugins/vitals/PluginVitalTemperature.java
+++ markusbro/markusbro-core/src/main/java/org/vg/markusbro/core/service/plugins/vitals/PluginVitalTemperature.java
@@ -1,22 +1,37 @@
 package org.vg.markusbro.core.service.plugins.vitals;
 
+import com.itextpdf.text.DocumentException;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.vg.markusbro.core.Utils;
 import org.vg.markusbro.core.service.plugins.Context;
+import org.vg.markusbro.core.service.plugins.vitals.report.VitalReportGenerator;
+import org.vg.markusbro.core.service.plugins.vitals.report.VitalReportParams;
 
-import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static org.vg.markusbro.core.Utils.parseDouble;
 
 public abstract class PluginVitalTemperature extends PluginVital {
 
     private static final String KEY_VITAL_TEMPERATURE = "temperature";
+    private static final String KEY_SELECT_PERIOD = "PluginVitalTemperatureView:pick_step";
+
+    // UI
+    protected final String OPTION_PERIOD_LAST_WEEK = getResource("report.period.lastWeek");
+    protected final String OPTION_PERIOD_LAST_MONTH = getResource("report.period.lastMonth");
+    protected final String OPTION_PERIOD_ALL = getResource("report.period.all");
+    protected final String OPTION_CANCEL = getResource("report.cancel");
 
     @Override
     public String getId() {
@@ -132,7 +147,7 @@ public abstract class PluginVitalTemperature extends PluginVital {
     }
 
     @Component
-    public static class PluginVitalTemperatureView extends PluginVitalTemperature {
+    public static class PluginVitalTemperatureView_step1 extends PluginVitalTemperature {
 
         @Override
         public double getScore(Context context) {
@@ -146,16 +161,102 @@ public abstract class PluginVitalTemperature extends PluginVital {
             final List<TemporalVital> entries = getVitals(context, KEY_VITAL_TEMPERATURE);
 
             if (!entries.isEmpty()) {
-                final SimpleDateFormat format = Utils.getDateTimeFormatter();
+                context.replyWithOptions(getResource("report.selectPeriod"), Arrays.asList(
+                        OPTION_PERIOD_LAST_WEEK,
+                        OPTION_PERIOD_LAST_MONTH,
+                        OPTION_PERIOD_ALL,
+                        OPTION_CANCEL
+                ));
 
-                final String result = entries.stream()
-                        .sorted(Comparator.comparing(TemporalVital::time))
-                        .map(e -> format.format(e.time()) + " " + e.value() + " C")
-                        .collect(Collectors.joining("\n"));
-
-                context.reply(String.format("Here are all your temperature measurements (%d):\n\n%s", entries.size(), result));
+                writeValue(context, KEY_SELECT_PERIOD, System.currentTimeMillis());
             } else {
                 context.reply(getResource("response.nothingFound"));
+            }
+        }
+    }
+
+    @Slf4j
+    @Component
+    public static class PluginVitalTemperatureView_step2 extends PluginVitalTemperature {
+
+        @Override
+        public double getScore(Context context) {
+            final String value = readValue(context, KEY_SELECT_PERIOD);
+            if (value == null) {
+                return SCORE_NEVER;
+            }
+
+            removeValue(context, KEY_SELECT_PERIOD);
+            return SCORE_ALWAYS;
+        }
+
+        @Override
+        public void handle(Context context) {
+            if (OPTION_PERIOD_LAST_WEEK.equals(context.getMessage())) {
+                sendReport(context, Utils.getDateLastWeek(), null);
+            } else if (OPTION_PERIOD_LAST_MONTH.equals(context.getMessage())) {
+                sendReport(context, Utils.getDateLastMonth(), null);
+            } else if (OPTION_PERIOD_ALL.equals(context.getMessage())) {
+                sendReport(context, null, null);
+            } else {
+                context.removeOptions(getResource("report.cancelled"));
+            }
+        }
+
+        private void sendReport(Context context, Date start, Date end) {
+            final List<TemporalVital> entries = getVitals(context, KEY_VITAL_TEMPERATURE, start, end);
+
+            if (entries.isEmpty()) {
+                context.removeOptions(getResource("response.nothingFound"));
+                return;
+            }
+
+            final Date actualStart = start != null
+                    ? start
+                    : entries.stream().map(TemporalVital::time).min(Date::compareTo).get();
+
+            final Date actualEnd = end != null
+                    ? end
+                    : entries.stream().map(TemporalVital::time).max(Date::compareTo).get();
+
+            final TemporalVital min = entries.stream()
+                    .min(Comparator.comparing(e -> Utils.parseDouble(e.value())))
+                    .get();
+
+            final TemporalVital max = entries.stream()
+                    .max(Comparator.comparing(e -> Utils.parseDouble(e.value())))
+                    .get();
+
+            final String average = String.format("%.1f", entries.stream()
+                    .mapToDouble(e -> Utils.parseDouble(e.value()))
+                    .average()
+                    .getAsDouble());
+
+            File tempFile = null;
+            try {
+                tempFile = Files.createTempFile(String.valueOf(context.getUserId()), "").toFile();
+
+                VitalReportGenerator.generateReport(tempFile, VitalReportParams.builder()
+                        .reportTitle(getResource("report.title"))
+                        .reportPeriodStart(actualStart)
+                        .reportPeriodEnd(actualEnd)
+                        .minValue(min)
+                        .maxValue(max)
+                        .averageValue(average)
+                        .data(entries)
+                        .userId(context.getUserId())
+                        .userName(context.getUserName())
+                        .build());
+
+                final String timestamp = LocalDateTime.now().format(Utils.getFileNameFormatter());
+                final String fileName = getResource("report.title") + " (" + timestamp + ").pdf";
+                context.sendFile(tempFile, fileName, true);
+            } catch (IOException | DocumentException e) {
+                log.error(e.getMessage(), e);
+            } finally {
+                if (tempFile != null) {
+                    tempFile.deleteOnExit();
+                }
             }
         }
     }
